79367aa9d71e038aa8133da927a68c29360d7293
andylamax <andylamax@programmer.net>
2020-12-15 07:05:43 +0300

    Initial commit

---
+++ buildSrc/src/main/kotlin/versions.kt
@@ -0,0 +1,21 @@
+object vers {
+    val kotlin = "1.4.20"
+    val nexus_staging = "0.22.0"
+
+    object kotlinx {
+        val datetime = "0.1.1"
+        val coroutines = "1.4.2"
+    }
+
+    object androidx {
+        val lifecycle = "2.2.0"
+    }
+
+    object asoft {
+        val viewmodel = "0.0.1"
+        val logging = "0.0.1"
+        val reakt = "0.0.7"
+        val builders = "1.1.1"
+        val test = "1.0.1"
+    }
+}
